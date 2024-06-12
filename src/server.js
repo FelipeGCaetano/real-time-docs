b871939652ad9d142905dc29ad0fcfbb13083194
Felipe Gabriel <felipecaetano2003@gmail.com>
2024-06-12 10:56:20 -0300

    Create socket IO server and connect client

--- src/server.js
+++ src/server.js
@@ -2,17 +2,26 @@ import express from 'express'
 import { config } from 'dotenv'
 import url from 'url'
 import path from 'path'
+import http from 'http'
+import { Server } from 'socket.io'
 
-config()
+config();
 
 const app = express();
 const port = process.env.PORT;
 
 const currentPath = url.fileURLToPath(import.meta.url);
 const publicDir = path.join(currentPath, '../..', 'public');
-
 app.use(express.static(publicDir));
 
-app.listen(port, () => 
+const httpServer = http.createServer(app);
+
+httpServer.listen(port, () => 
     console.log(`Server running on port ${port}`)
-)
+);
+
+const io = new Server(httpServer);
+
+io.on("connection", () => {
+    console.log('Client connected');
+})
