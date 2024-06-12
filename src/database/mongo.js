e567e7b0cf59bb7d841323de28c5f1dd7c37c7d6
Felipe Gabriel <felipecaetano2003@gmail.com>
2024-06-12 12:52:53 -0300

    Create and connect MongoDB

---
+++ src/database/mongo.js
@@ -0,0 +1,20 @@
+import { MongoClient } from "mongodb";
+import { config } from "dotenv";
+config()
+
+const client = new MongoClient(process.env.MONGO_URL);
+
+let docCollection;
+
+try {
+    await client.connect();
+    
+    const db = client.db("database");
+    docCollection = db.collection("dbdocuments");
+
+    console.log("MongoDB connected!");
+} catch (error) {
+    console.log(error);
+}
+
+export default client
